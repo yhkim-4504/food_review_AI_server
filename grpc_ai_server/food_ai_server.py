e0052d17dfa484b711f5cfc5d1c46093ea7e8f3a
yhkim <ky45042@naver.com>
2022-11-24 21:59:02 +0900

    큐이용 다중요청 처리기능 추가

--- grpc_ai_server/food_ai_server.py
+++ grpc_ai_server/food_ai_server.py
@@ -2,24 +2,45 @@ import numpy as np
 import cv2
 import grpc
 from concurrent import futures
-from grpc_ai_server import food_classification_pb2
-from grpc_ai_server import food_classification_pb2_grpc
+from . import food_classification_pb2
+from . import food_classification_pb2_grpc
 from model.predictor import FoodPredictor
+from collections import deque
+from time import sleep
+from torch import cuda
 
+QUEUE_WAITING_TIME = 0.001
 
 class FoodAiServicer(food_classification_pb2_grpc.FoodAiServicer):
     print('Food Classification Model loading...', end='')
     food_predictor = FoodPredictor('./model/Base2_chkpoint_jit_script.pt', (256, 256), 'cuda:0')
+    waiting_queue = deque()
+    process_id = 0
     print('Done.')
 
     def CheckGpuStatus(self, request, context):
         print('CheckGpu grpc')
-        return food_classification_pb2.GpuStatus(status=True)
+
+        if cuda.is_available():
+            status = True
+        else:
+            status = False
+
+        return food_classification_pb2.GpuStatus(status=status)
     
     def PredictFoodImage(self, request, context):
         print('PredictFoodImage grpc')
+        current_process_id = self.process_id
+        self.process_id += 1
+        self.waiting_queue.append(current_process_id)
+
+        while self.waiting_queue[0] != current_process_id:
+            sleep(QUEUE_WAITING_TIME)
+
         image = cv2.imdecode(np.frombuffer(request.image, np.uint8), cv2.IMREAD_COLOR)
         result = self.food_predictor.inference(image)
+        self.waiting_queue.popleft()
+        print(current_process_id, 'Done')
 
         return food_classification_pb2.PredictionResult(food_type=result['index'], probability=result['prob'])
 
@@ -29,7 +50,8 @@ def serve():
     port = '50051'
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     food_classification_pb2_grpc.add_FoodAiServicer_to_server(FoodAiServicer(), server)
-    server.add_insecure_port('127.0.0.1:' + port)  # localhost
+    server.add_insecure_port('0.0.0.0:' + port)  # localhost
+    #server.add_insecure_port('[::]:' + port)  # localhost
     server.start()
     print("Server started, listening on " + port)
     server.wait_for_termination()
